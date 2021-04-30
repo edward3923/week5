e3c4e75d0e57017c4c1e4b5387f1224e34d83524
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-04-29 21:07:37 -0500

    lab5

--- code-along/events-practice.js
+++ code-along/events-practice.js
@@ -9,29 +9,57 @@ function getRandomInt(max) {
 // RECIPE (algorithm) – do not modify the HTML!
 
 // Store a reference to the roll button in memory
+let rollButton = document.querySelector(`#roll-button`)
 
 // Listen for the clicking of the roll button, when clicked:
-
+rollButton.addEventListener(`click`, async function(event) {
   // - Ignore the roll button's default behavior
+  event.preventDefault()
 
   // - Store a random integer in memory, representing the roll of the first die
-  
+  let firstDie = getRandomInt(6)
+
   // - Store a random integer in memory, representing the roll of the second die
+  let secondDie = getRandomInt(6)
 
   // - Store the total of the two random numbers, representing the total roll
+  let totalDie = firstDie + secondDie
 
   // - Replace the first die image using the first random value (hint: setAttribute)
+  let image1 = document.querySelector(`.die1`)
+  image1.setAttribute(`src`, `../images/dice/${firstDie}.png`)
 
   // - Replace the second die image using the second random value (hint: setAttribute)
+  let image2 = document.querySelector(`.die2`)
+  image2.setAttribute(`src`, `../images/dice/${secondDie}.png`)
 
   // - Store a reference to the player name input element
+  let playerName = document.querySelector(`#player`)
 
   // - Grab the value of the player name element and store the player's name in memory
+  let player = playerName.value
+  //console.log(firstName)
+
+  let greetElement = document.querySelector(`.result`)
+
+    // - Make sure the player's name is filled out; if it is:
+    if (player.length > 0) {
+      let sentence = `Hi, ${player}. Your total was ${totalDie}.`
+      greetElement.insertAdjacentHTML(`beforeend`, `
+      <li>${sentence}</li>
+    `)
+    }
+    else {
+      greetElement.insertAdjacentHTML(`beforeend`, ``)
+    }
   
-  // - Make sure the player's name is filled out; if it is:
+
+
 
     // - Form a sentence in memory, containing the player's name and the total that was rolled
 
     // - Store a reference to the "result" un-ordered list element
 
     // - Insert the sentence to the "result" un-ordered list as a list item (li)
+
+  })
--- lab/week5.js
+++ lab/week5.js
@@ -12,29 +12,75 @@
 //        Weather for Chicago, Illinois".
 // - Homework: Complete the application by accepting a number of days; show the current weather 
 //             conditions and forecast based on the number of days entered by the user.
+// 377d61759c2a4e0eb7611616213004
+
 
 window.addEventListener('DOMContentLoaded', async function() {
+
+
+
   // Get a reference to the "get weather" button
+  let getWeatherButton = document.querySelector('.get-weather')
+  
 
   // When the "get weather" button is clicked:
+  getWeatherButton.addEventListener(`click`, async function(event) {
 
     // - Ignore the default behavior of the button
+    event.preventDefault()
 
     // - Get a reference to the element containing the user-entered location
+    let locationInput = document.querySelector(`#location`)
 
     // - Get the user-entered location from the element's value
+    let location = locationInput.value
 
     // - Check to see if the user entered anything; if so:
-
+    if (location.length > 0) {
       // - Construct a URL to call the WeatherAPI.com API
-
       // - Fetch the url, wait for a response, store the response in memory
-
+      let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=377d61759c2a4e0eb7611616213004&q=${location}&days=3`)
       // - Ask for the json-formatted data from the response, wait for the data, store it in memory
+      let json = await response.json()
 
       // - Write the json-formatted data to the JavaScript console
+      console.log(json)
 
       // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+      let interpretedLocation = `${json.location.name}, ${json.location.region}`
+      console.log(interpretedLocation)
+      
+      let currentTemperature = `Current temperature is ${json.current.temp_f}`
+      console.log(currentTemperature)
+
+      let forecast = json.forecast.forecastday
+      console.log(forecast)
+
+      let currentWeather = document.querySelector(`.current`)
+      currentWeather.innerHTML = `
+      <div class="text-center space-y-2">
+        <div class="font-bold text-3xl">Current Weather for ${interpretedLocation}</div>
+        <div class="font-bold">
+          <img src="https:${json.current.condition.icon}" class="inline-block">
+          <span class="temperature">${json.current.temp_f}</span>° 
+          and
+          <span class="conditions">${json.current.condition.text}</span>
+        </div>
+      </div>
+    `
+
+
+    }
+    else {
+      // Write an else statement here
+    }
+
+
+
+
+      
 
       // - Continue the recipe yourself!
+          
+  })
 })
